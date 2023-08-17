139539f57a83f7e1058b9b512793db1ae4710981
eduardagnz <eduarda.oliveira@sou.inteli.edu.br>
2023-08-17 17:54:32 -0300

    feat: config inicial front

--- src/frontend/project-ep/src/App.js
+++ src/frontend/project-ep/src/App.js
@@ -1,25 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+// import { telaExemplo } from "./pages/exemplo";
+import  ButtonUsage  from "C:/Users/Inteli/Desktop/ProjetoM5/Grupo-03/src/frontend/project-ep/src/components/atoms/botao.tsx"
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+  function App() {
+    return (
+      <>
+        <h1>NHAAA</h1>
+        <ButtonUsage/>
+        {/* <telaExemplo/> */}
+      </>
+    );
+  }
 
-export default App;
+  export default App;

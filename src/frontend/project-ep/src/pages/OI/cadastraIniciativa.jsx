b326a11ce761a74f8b0821e8c9cac28e208c7f9e
YuriFAToledo <yuri.toledo@sou.inteli.edu.br>
2023-08-18 09:36:32 -0300

    feat: cadastra iniciativa com state

--- src/frontend/project-ep/src/pages/OI/cadastraIniciativa.jsx
+++ src/frontend/project-ep/src/pages/OI/cadastraIniciativa.jsx
@@ -1,13 +1,33 @@
-import React from "react";
-// import {navbar} from "../components/atoms/navbar";
+import React, { useState, useEffect } from "react";
+import Navbar from "../../components/atoms/navbar.tsx";
+import ButtonUsage from "../../components/atoms/botao.tsx";
 // import {telaFundo} from "../components/";
 
 const CadastraIniciativa = (props) => {
+
+    const [problemaEnviado, setProblemaEnviado] = useState(false);
+
     return (
         <>
+            <Navbar/>
+            {problemaEnviado ? 
+            <div>
+                <h3>Nurinuri</h3>
+                <button onClick={() => setProblemaEnviado(false)}>VOLTAR</button>
+            </div>
+            :
+            <div >
 
-            <h1>NHEI</h1>
-            <h3>NhUI</h3>
+                <h1>Enviar proposta de projeto</h1>
+                <h6>Descrição do problema:</h6>
+                <input type="text" />
+                <ButtonUsage/>
+                <button onClick={() => setProblemaEnviado(true)}>AVANÇAR</button>
+            
+            </div>
+            }
+
+            
 
         </>
     )

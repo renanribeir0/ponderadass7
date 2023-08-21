ab1666e5ff2f2382e3512dfb5cfe676b0e1d9e98
eduardagnz <eduarda.oliveira@sou.inteli.edu.br>
2023-08-21 16:21:42 -0300

    Add files

---
+++ src/frontend/project-ep/src/pages/OI/cadastroOI/index.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+import Navbar from "../../../components/atoms/navbar/index.jsx";
+import ButtonUsage from "../../../components/atoms/botao.tsx";
+import Card from '../../../components/molecules/card/index.jsx'
+// import styles from '../cadastraIniciativa/styles.module.scss';
+// import {telaFundo} from "../components/";
+import TextField from '@mui/material/TextField';
+import desenho from '../../../assets/desenho.png';
+

c3bf6d9bc752a7b4835fcd387d28c5c65465d592
MauricioTFelicissimo <mauricio.felicissimo@sou.inteli.edu.br>
2023-08-22 16:35:03 -0300

    feat: endpoint parceiro importado no appModule

--- src/backend/backend-montech/src/app.module.ts
+++ src/backend/backend-montech/src/app.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { IniciativasModule } from './iniciativas/iniciativas.module';
 import { TabelaGenericaModule } from './tabelaGenerica/tabelaGenerica.module';
+import { ParceiroModule } from './parceiro/parceiro.module';
 
 @Module({
-  imports: [IniciativasModule, TabelaGenericaModule],
+  imports: [IniciativasModule, TabelaGenericaModule, ParceiroModule],
   controllers: [],
   providers: [],
 })

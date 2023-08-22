42abe53ee9af1c2ffbddb3847ff658525ae005d9
MauricioTFelicissimo <110629792+MauricioTFelicissimo@users.noreply.github.com>
2023-08-22 17:46:16 -0300

    Revert "feat: endpoint parceiro"
    This reverts commit a4fa83369c9937b2c59c3ba39ec482c8eb76979b.

--- src/backend/backend-montech/src/parceiro/parceiro.module.ts
+++
@@ -1,8 +0,0 @@
-import { Module } from "@nestjs/common"
-import { ParceiroController } from './parceiro.controller'
-
-@Module({
-    controllers: [ParceiroController],
-    providers: []
-})
-export class ParceiroModule {}

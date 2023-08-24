b63bd88ad857df8c5c4e0d60257d1a60fe89e376
eduardagnz <eduarda.oliveira@sou.inteli.edu.br>
2023-08-23 23:32:31 -0300

    Alterações

--- src/frontend/project-ep/src/pages/OI/cadastroOI/index.jsx
+++ src/frontend/project-ep/src/pages/OI/cadastroOI/index.jsx
@@ -2,8 +2,30 @@ import React from "react";
 import Navbar from "../../../components/atoms/navbar/index.jsx";
 import ButtonUsage from "../../../components/atoms/botao.tsx";
 import Card from '../../../components/molecules/card/index.jsx'
-// import styles from '../cadastraIniciativa/styles.module.scss';
-// import {telaFundo} from "../components/";
 import TextField from '@mui/material/TextField';
 import desenho from '../../../assets/desenho.png';
+import style from './style.module.scss';
+import styles from '../cadastraIniciativa/styles.module.scss';
 
+export const Cadastro = (props) => {
+    
+    // const [problemaEnviado, setProblemaEnviado] = useState(false);
+    // const [] = useState(true);
+    // //Crair as varias para preenchimento: Nome, CNPJ, telefone, email, etc
+    // const [descricao, setDescricao] = useState("");  // Novo estado para armazenar a descrição
+    // const [selectedCard, setSelectedCard] = useState(null); // Estado para armazenar o Card selecionado
+
+ return (
+    <>
+        <div className={style.cadastroGeral}>
+            <div className={styles.header}>
+            <Navbar/>
+            </div>
+            <div className={styles.botao}>
+                 <ButtonUsage/>
+            </div>
+                 
+        </div>
+    </>
+    )
+}

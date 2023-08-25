a8ed4062264d029b479ec17fd72341e1b9df1363
YuriFAToledo <yuri.toledo@sou.inteli.edu.br>
2023-08-25 11:48:02 -0300

    api do gpt parcial + mockado em array

--- src/backend/backend-montech/src/modulos/modulos.controller.ts
+++ src/backend/backend-montech/src/modulos/modulos.controller.ts
@@ -9,13 +9,39 @@ export class ModulosController {
     //Cria o Get universal de acesso a url
     @Get()
     nomeDaFuncao() {
-        const modulo = {
-            id: 69,
-            descricao: "Construção de lógica para predição com inteligência artificial",
-            competencias: ["Interpretar e elaborar planos de negócios: 'Planos de negócios'", "Traduzir a estratégia em planos de marketing e comunicação: 'Branding e comunicação'", "Idealizar estruturas organizacionais: 'Cultura organizacional'", "Idealizar estruturas organizacionais: 'Governança corporativa'", "Decidir sobre investimentos, financiamentos e gestão de recursos: 'Ciclo de funding em startups'"],
-            iniciativasId: 0 
-        }
-        return modulo
+        const modulos = [{
+            
+                id: 2,
+                nomeModulo: "Módulo 02 - Ciclo Básico",
+                descricao: "Desenvolvimento de uma plataforma Web",
+                competencias: [
+                    "Identificar_tendências e oportunidades de mercado: 'Frameworks(Oceano Azul, 5 forças, 4P's, 6D)'",
+                    "Interpretar_e_elaborar_planos_de_negócios : 'Incubadoras e ecossistema de Startups'",
+                    "Traduzir_a_estratégia_em_planos_de_marketing_e_comunicação: 'Pesquisa de mercado e tendências'",
+                    "Elaborar_orçamentos_e_fluxos_de_caixa: 'Fluxo de Caixa, juros simples e compostos'",
+                    "Elaborar_orçamentos_e_fluxos_de_caixa: 'Taxa de desconto, IRR, NPV, PV e FV'"
+    
+                ],
+            },
+
+            {
+                id: 3,
+                nomeModulo: "Módulo 03 - Ciclo Básico",
+                descricao: "Construção de lógica para predição com inteligência artificial",
+                competencias: [
+                "Interpretar e elaborar planos de negócios: 'Planos de negócios'", 
+                "Traduzir a estratégia em planos de marketing e comunicação: 'Branding e comunicação'", 
+                "Idealizar estruturas organizacionais: 'Cultura organizacional'", 
+                "Idealizar estruturas organizacionais: 'Governança corporativa'", 
+                "Decidir sobre investimentos, financiamentos e gestão de recursos: 'Ciclo de funding em startups'"
+                ],
+                
+                
+            },
+        
+        
+        ]
+        return modulos
     }
 
     //127.0.0.1:3000/parceiro/<conteudo>

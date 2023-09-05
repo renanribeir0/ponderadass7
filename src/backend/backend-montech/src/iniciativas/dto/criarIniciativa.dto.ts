71fdbbfeae40d5c49080805adee3a0fe08cca5e4
YuriFAToledo <yuri.toledo@sou.inteli.edu.br>
2023-09-05 19:19:17 -0300

    fix: schema atualizado + bd na RDS

--- src/backend/backend-montech/src/iniciativas/dto/criarIniciativa.dto.ts
+++ src/backend/backend-montech/src/iniciativas/dto/criarIniciativa.dto.ts
@@ -1,11 +1,10 @@
-import { Modulo, Parceiro, Turma } from "@prisma/client";
-
 export class CriaIniciativaDto {
   moduloId: number;
   parceiroId: number;
   problema: string;
   escopo: string;
-  mvp: string[];
+  status: string;
+  mvp: string;
   tema: string;
   turmaId?: number;
 }
